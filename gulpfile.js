ef5d84373a5869b03fa26d396aa66879cf24efe5
linghuyong <linghuyong@hotmail.com>
2017-10-24 12:14:41 +0800

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 
 var inlinesource = require('gulp-inline-source');
 var webserver = require('gulp-webserver');
+var postcss = require('gulp-postcss');
+var adaptive = require('postcss-adaptive');
  
 gulp.task('inlinesource', function () {
     var options = {
@@ -10,6 +12,13 @@ gulp.task('inlinesource', function () {
  
     return gulp.src('./src/*.html')
         .pipe(inlinesource(options))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('postcss', function () {
+    var processors = [adaptive({ remUnit: 75 })];
+    return gulp.src('./src/*.css')
+        .pipe(postcss(processors))
         .pipe(gulp.dest('./dist'));
 });
 
@@ -22,6 +31,6 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('default', ['inlinesource'], function() {
+gulp.task('default', ['inlinesource', 'postcss'], function() {
 
 });
